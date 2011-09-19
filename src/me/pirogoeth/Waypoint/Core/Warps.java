37097316500ffcf54c6f0b98872e76646d204e3e
MarauderIIC <marauderiicspambox@gmail.com>
2011-09-19 02:00:00 -0300

    Pitch/yaw warp

--- src/me/pirogoeth/Waypoint/Core/Warps.java
+++ src/me/pirogoeth/Waypoint/Core/Warps.java
@@ -1,5 +1,7 @@
 package me.pirogoeth.Waypoint.Core;
 
+import java.lang.Float;
+
 import java.util.List;
 import java.util.Iterator;
 import java.util.ArrayList;
@@ -17,7 +19,6 @@ public class Warps {
     public static Waypoint plugin;
     public static Config c;
     public static Configuration config;
-    public static Configuration main;
     public static Logger log = Logger.getLogger("Minecraft");
     public List<String> groups;
     public Warps (Waypoint instance)
@@ -25,7 +26,6 @@ public class Warps {
         plugin = instance;
         c = plugin.config;
         config = c.getWarp();
-        main = c.getMain();
     }
     public void LoadGroups ()
     {
@@ -57,11 +57,15 @@ public class Warps {
         double x = l.getX();
         double y = l.getY();
         double z = l.getZ();
+    	double yaw = (double)l.getYaw();
+		double pitch = (double)l.getPitch();
         String worldname = p.getWorld().getName().toString();
         String owner = p.getName().toString();
         config.setProperty(WarpNode(warpname, "coord.X"), x);
         config.setProperty(WarpNode(warpname, "coord.Y"), y);
         config.setProperty(WarpNode(warpname, "coord.Z"), z);
+		config.setProperty(WarpNode(warpname, "coord.yaw"), yaw);
+		config.setProperty(WarpNode(warpname, "coord.pitch"), pitch);
         config.setProperty(WarpNode(warpname, "world"), worldname);
         config.setProperty(WarpNode(warpname, "owner"), owner);
         String permission = (String)groups.get(0);
@@ -123,67 +127,32 @@ public class Warps {
         double x = (Double) config.getProperty(WarpNode(warpname, "coord.X"));
         double y = (Double) config.getProperty(WarpNode(warpname, "coord.Y"));
         double z = (Double) config.getProperty(WarpNode(warpname, "coord.Z"));
+		double pitch = 0;
+		double yaw = 0;
+		boolean basic = false;
+		
+		try
+		{
+			pitch = (Double) config.getProperty(WarpNode(warpname, "coord.pitch"));
+			yaw = (Double) config.getProperty(WarpNode(warpname, "coord.yaw"));
+		}
+		catch (Exception e)
+		{
+			basic = true;
+		}
+		
         World w = plugin.getServer().getWorld(worldname);
-        Location l = new Location(w, x, y, z);
+		Location l;
+		if (basic)
+		{
+			l = new Location(w, x, y, z);
+		}
+		else
+		{
+			l = new Location(w, x, y, z, (float) yaw, (float) pitch);
+		}
         p.teleport(l);
-        // warpstring stuff :: 1.6
-        if (((String) main.getProperty("warp.warpstring_enabled")).equalsIgnoreCase("true"))
-        {
-            String warpstring = (String) main.getProperty("warp.string");
-            if (warpstring == null)
-            {
-                // if only.
-                log.warning("[Waypoint] Warp string is null! Please set the warp.string value in config.yml");
-                return true;
-            }
-           warpstring = warpstring.replaceAll("%w", "" + warpname);
-           warpstring = warpstring.replaceAll("%p", "" + p.getName().toString());
-           p.sendMessage(String.format("%s%s", ChatColor.BLUE, warpstring));
-           return true;
-        }
-        return true;
-    }
-    public boolean RemoteWarp (Player target, Player sender, String warpname)
-    {
-        if (config.getProperty(WarpNode(warpname, "world")) == null)
-        {
-            sender.sendMessage(ChatColor.RED + "[Waypoint] Warp " + warpname + " does not exist.");
-            return false;
-        }
-        String permission = (String) config.getProperty(WarpNode(warpname, "permission"));
-        if (checkperms(sender, permission) == false)
-        {
-            sender.sendMessage(ChatColor.RED + "[Waypoint] You do not have permissions to access this warp.");
-            return true;
-        }
-        String worldname = (String) config.getProperty(WarpNode(warpname, "world"));
-        if (!target.getWorld().toString().equals(worldname) && ((String) plugin.config.getMain().getProperty("warp.traverse_world_only")).equals("true"))
-        {
-            sender.sendMessage(ChatColor.RED + "[Waypoint] You are not allowed to warp others between worlds.");
-            return true;
-        }
-        double x = (Double) config.getProperty(WarpNode(warpname, "coord.X"));
-        double y = (Double) config.getProperty(WarpNode(warpname, "coord.Y"));
-        double z = (Double) config.getProperty(WarpNode(warpname, "coord.Z"));
-        World w = plugin.getServer().getWorld(worldname);
-        Location l = new Location(w, x, y, z);
-        target.teleport(l);
-        sender.sendMessage(ChatColor.GREEN + String.format("[Waypoint] %s has been warped to %s", target.getName().toString(), warpname));
-        // warpstring stuff :: 1.6
-        if (((String) main.getProperty("warp.warpstring_enabled")).equalsIgnoreCase("true"))
-        {
-            String warpstring = (String) main.getProperty("warp.string");
-            if (warpstring == null)
-            {
-                // if only.
-                log.warning("[Waypoint] Warp string is null! Please set the warp.string value in config.yml");
-                return true;
-            }
-            warpstring = warpstring.replaceAll("%w", "" + warpname);
-            warpstring = warpstring.replaceAll("%p", "" + target.getName().toString());
-            target.sendMessage(String.format("%s%s", ChatColor.BLUE, warpstring));
-            return true;
-        }
+        p.sendMessage(ChatColor.AQUA + "Welcome to " + warpname + ", " + p.getName().toString());
         return true;
     }
 }
