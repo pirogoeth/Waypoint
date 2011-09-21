623f54f8235781b4f50c3450bd90b67ca895af29
pirogoeth <miyoko@maio.me>
2011-09-21 17:29:55 -0500

    pretty much finished writing the Registry, nothing else comes to mind.

--- src/me/pirogoeth/Waypoint/Util/Registry.java
+++ src/me/pirogoeth/Waypoint/Util/Registry.java
@@ -13,6 +13,22 @@ import org.bukkit.command.CommandSender;
 
 // Core imports
 import me.pirogoeth.Waypoint.Waypoint;
+import me.pirogoeth.Waypoint.Util.Command;
+
+private class RegistryError extends Exception {
+    protected String error;
+    private RegistryError () {
+        super();
+        error = "An unknown error occurred.";
+    }
+    private RegistryError (String err) {
+        super(err);
+        error = err;
+    }
+    public static String getError () {
+        return error;
+    }
+}
 
 protected class Registry {
     // important core registry variables.
@@ -68,13 +84,23 @@ protected class Registry {
     protected static Object put (Object K, Object V) {
         /**
          * Puts a key-value pair into the command map.
-         * This is not meant to be a replacement for the 
+         * This is not meant to be a replacement for the
          *     `register` method.
          *
          * [accepts: (Object) K, (Object) V;
          *      returns: previous V or null]
          */
         return (Object) command.put((Object) K, (Object) V);
+    }
+    private static Object remove (Object K) {
+        /**
+         * Removes K from the command map and returns
+         *     the previous value for K, or null, if
+         *     no such value existed.
+         *
+         * [accepts: (Object) K; returns: Object]
+         */
+        return (Object) command.remove((Object) K);
     }
     public static boolean isEmpty () {
         /**
@@ -94,5 +120,63 @@ protected class Registry {
          */
         return (Set<String>) command.keySet();
     }
-    // to be continued...
+    // core registration methods
+    public static boolean registerCommand (String commandLabel, Command commandInst) {
+        /**
+         * Allows for the registering of a command in the
+         *     registry's internal map to be process by the main onCommand method.
+         *
+         * [accepts: (String) commandLabel, (Command) commandInst; returns: boolean]
+         */
+        // is this command already registered?
+        if (containsKey((Object) commandLabel))
+            throw new RegistryError(String.format("Command [%s] is already registered.", commandLabel));
+        // does the map return null when we place the new value?
+        if (put((Object) commandLabel, (Object) commandInst) == null)
+            return true;
+        else
+            return false;
+    }
+    public static boolean deregisterCommand (String commandLabel) {
+        /**
+         * Allows for the deregistration of a command from the
+         *     registry's internal map.
+         *
+         * [accepts: (String) commandLabel; returns: boolean]
+         */
+        // is the command actually registered?
+        if (!(containsKey((Object) commandLabel)))
+            throw new RegistryError(String.format("Command [%s] is not registered.", commandLabel));
+        // does the map actually return a value for commandLabel, or does it return null?
+        if (remove((Object) commandLabel) == null || remove((Object) commandLabel != null)
+            return true;
+        else
+            return false;
+    }
+    public static void deregisterAll () {
+        /**
+         * Allows for the deregistration of all registered
+         *     commands in a quick and easy fashion.
+         *
+         * [accepts: none; returns: none]
+         */
+        // well, this is boring.
+        command.clear();
+        return;
+    }
+    public static Command processCommand (String commandLabel) {
+        /**
+         * Processes the incoming command and returns the
+         *     value for commandLabel, if it exists.
+         *
+         * [accepts: (String) commandLabel; returns: Command]
+         */
+        // does this key even exist?
+        if (!(containsKey((Object) commandLabel)))
+            return null;
+        else if (containsKey((Object) commandLabel))
+            return (Command) get((Object) commandLabel);
+        else
+            return null;
+    }
 }
