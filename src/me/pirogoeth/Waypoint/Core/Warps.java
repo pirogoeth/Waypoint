5b3ac61695628b958517e20798e68662cea8bf1e
Shaun <shaun@hackr.info>
2012-01-29 16:47:33 -0600

    1.6.5: oops, called getBoolean incorrectly.

--- src/me/pirogoeth/Waypoint/Core/Warps.java
+++ src/me/pirogoeth/Waypoint/Core/Warps.java
@@ -118,7 +118,7 @@ public class Warps {
         }
         Location l = new Location(w, x, y, z);
         p.teleport(l);
-        if (((String)main.getString("warp.warpstring_enabled")).equalsIgnoreCase("true") || main.getBoolean("warp.warpstring_enabled") == true) {
+        if (((String)main.getString("warp.warpstring_enabled")).equalsIgnoreCase("true") || main.getBoolean("warp.warpstring_enabled", false) == true) {
             String warpstring = (String)main.getString("warp.string");
             if (warpstring == null) {
                 log.warning("[Waypoint] Warp string is null! Please set the warp.string value in config.yml");
