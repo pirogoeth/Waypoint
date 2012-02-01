9fcc45bb215dd486d296756332b40912268f3f9d
Shaun <shaun@hackr.info>
2012-02-01 00:10:01 -0600

    1.6.5-tool: added compile.sh

---
+++ compile.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+version=`cat src/plugin.yml | grep version | awk '{print $2}'`
+
+echo "[Waypoint(${version})] building.]"
+
+javac -g -cp inc/craftbukkit.jar:inc/permissions.jar:inc/bukkit.jar:inc/vault.jar \
+    src/me/pirogoeth/Waypoint/*.java src/me/pirogoeth/Waypoint/Core/*.java \
+    src/me/pirogoeth/Waypoint/Commands/*.java src/me/pirogoeth/Waypoint/Events/*.java \
+    src/me/pirogoeth/Waypoint/Util/*.java
+
+jar cf Waypoint-${version}.jar
